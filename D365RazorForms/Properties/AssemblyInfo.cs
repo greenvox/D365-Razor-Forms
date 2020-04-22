3aa1c689b0fe06d57b7738e556f2812695c81918
Sarfraz Khan <Sarfraz.Khan@mcaconnect.com>
2020-04-21 21:34:03 -0500

    updated

--- D365RazorForms/Properties/AssemblyInfo.cs
+++ D365RazorForms/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("D365RazorForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("D365 Razor Forms")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MCA Connect")]
 [assembly: AssemblyProduct("D365RazorForms")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
